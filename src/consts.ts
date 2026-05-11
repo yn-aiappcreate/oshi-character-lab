801dfc41e2d69b3153cb61f979f356dae80808cd
yn-aiappcreate <kukt2012@gmail.com>
2026-05-11 12:42:28 +0900

    Create initial site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '推しキャラ遭遇研究所';
+export const SITE_DESCRIPTION = '推しキャラに会うためのキャラグリ・テーマパーク遠征・推し活旅行ガイド';
